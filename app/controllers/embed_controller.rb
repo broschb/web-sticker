c34373fd26c491eb99186bc4ca2fd13dbafd2fc8
Brandon Broschinsky <broschb@gmail.com>
2014-10-09 22:34:47 -0600

    iframe work

--- app/controllers/embed_controller.rb
+++ app/controllers/embed_controller.rb
@@ -1,6 +1,7 @@
 class EmbedController < ApplicationController
   skip_before_filter :authenticate_user!, :only => [:index]
   def index
+    response.headers.delete('X-Frame-Options')
     render :layout => false
   end
 end
